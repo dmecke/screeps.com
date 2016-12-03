ec8cd8aff696b3144afcd73d745bf00bf513d355
Daniel Mecke <daniel.mecke@cunningsoft.de>
2016-12-03 15:50:29 +0100

    removed unused fsm actions

--- src/Action/Action.ts
+++
@@ -1,7 +0,0 @@
-export abstract class Action_Action {
-    protected creep: Creep;
-    constructor(creep: Creep) {
-        this.creep = creep;
-    }
-    public abstract execute(target: RoomObject): void;
-}
