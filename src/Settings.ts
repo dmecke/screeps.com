4bd6bfb21cf89c77c1c846f3300e9daf0a60585c
Daniel Mecke <daniel.mecke@cunningsoft.de>
2016-12-04 10:10:36 +0100

    put the controller right next to the structure to avoid travelling

--- src/Settings.ts
+++ src/Settings.ts
@@ -3,7 +3,7 @@ export class Settings {
     public static ROAD_PLANNING_THRESHOLD = 1000;
     public static BUILD_PRIORITY_CONTAINER = 30;
     public static BUILD_PRIORITY_EXTENSION = 25;
-    public static BUILD_DISTANCE_CONTAINER = 2; // how far should a container be placed next to a structure?
+    public static BUILD_DISTANCE_CONTAINER = 1; // how far should a container be placed next to a structure?
 
     public static TREE_SUCCESS = 1;
     public static TREE_FAILURE = 2;
