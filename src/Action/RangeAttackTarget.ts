ef688594a5db60ba2b3e4ce229cd0f78eb9f752c
Daniel Mecke <daniel.mecke@cunningsoft.de>
2016-12-08 20:07:34 +0100

    added ranged attack ability to defenders

---
+++ src/Action/RangeAttackTarget.ts
@@ -0,0 +1,21 @@
+import {Tree_Core_Action} from "../Tree/Core/Action";
+import {Settings} from "../Settings";
+import {Tree_Core_Tick} from "../Tree/Core/Tick";
+
+export class Action_RangeAttackTarget extends Tree_Core_Action {
+
+    public tick(tick: Tree_Core_Tick): number {
+        let creep = tick.target as Creep;
+        let target = tick.blackboard.get("target", tick.tree.id) as Creep|Structure;
+
+        if (creep.rangedAttack(target) !== OK) {
+            return Settings.TREE_FAILURE;
+        }
+
+        if (target.hits === 0) {
+            return Settings.TREE_SUCCESS;
+        }
+
+        return Settings.TREE_RUNNING;
+    }
+}
--- src/Role/DefenderTree.ts
+++ src/Role/DefenderTree.ts
@@ -5,6 +5,7 @@ import {Check_HostileCreepInRoom} from "../Check/HostileCreepInRoom";
 import {Action_AssignNearestHostileCreepAsTarget} from "../Action/AssignNearestHostileCreepAsTarget";
 import {Action_MoveToTarget} from "../Action/MoveToTarget";
 import {Action_AttackTarget} from "../Action/AttackTarget";
+import {Action_RangeAttackTarget} from "../Action/RangeAttackTarget";
 
 export = new Tree_Tree(
     "Defender",
@@ -12,6 +13,7 @@ export = new Tree_Tree(
         new Check_HostileCreepInRoom(),
         new Action_AssignNearestHostileCreepAsTarget(),
         new Tree_Composite_Priority([
+            new Action_RangeAttackTarget(),
             new Action_AttackTarget(),
             new Action_MoveToTarget(),
         ]),
