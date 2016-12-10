65a36f2007d4a6cb3d94fd9e5b2bd8ccd379ace7
Daniel Mecke <daniel.mecke@cunningsoft.de>
2016-12-10 12:40:41 +0100

    refactored tower name and memory

--- src/Screeps/StructureTower.ts
+++ src/Screeps/StructureTower.ts
@@ -2,6 +2,15 @@ let loadStructureTowerPrototype = function() {
 
     const MEMORY_KEY_TOWERS = "towers";
 
+    Object.defineProperty(StructureTower.prototype, "name", {
+        get: function(this: StructureTower) {
+            return "Tower_" + this.id;
+        },
+        set: function(this: StructureTower) {
+            throw new Error();
+        },
+    });
+
     Object.defineProperty(StructureTower.prototype, "memory", {
         get: function(this: StructureTower) {
             if (_.isUndefined(Memory[MEMORY_KEY_TOWERS])) {
@@ -10,7 +19,7 @@ let loadStructureTowerPrototype = function() {
             if (!_.isObject(Memory[MEMORY_KEY_TOWERS])) {
                 return undefined;
             }
-            return Memory[MEMORY_KEY_TOWERS][this.id] = Memory[MEMORY_KEY_TOWERS][this.id] || {};
+            return Memory[MEMORY_KEY_TOWERS][this.name] = Memory[MEMORY_KEY_TOWERS][this.name] || {};
         },
         set: function(this: StructureTower, value) {
             if (_.isUndefined(Memory[MEMORY_KEY_TOWERS])) {
@@ -19,16 +28,12 @@ let loadStructureTowerPrototype = function() {
             if (!_.isObject(Memory[MEMORY_KEY_TOWERS])) {
                 throw new Error("Could not set tower memory.");
             }
-            Memory[MEMORY_KEY_TOWERS][this.id] = value;
+            Memory[MEMORY_KEY_TOWERS][this.name] = value;
         },
     });
 
     StructureTower.prototype.debug = function(this: StructureTower): boolean {
         return this.memory.debug;
-    };
-
-    StructureTower.prototype.constructor = function(this: StructureTower) {
-        this.name = "Tower " + this.id;
     };
 };
 
