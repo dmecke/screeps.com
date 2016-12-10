648ed94738942072b73ffcc307a34aa99de5f485
Daniel Mecke <daniel.mecke@cunningsoft.de>
2016-12-10 15:13:19 +0100

    fixed assignment of nearest creep

--- src/Action/AssignNearestHostileCreepAsTarget.ts
+++ src/Action/AssignNearestHostileCreepAsTarget.ts
@@ -3,6 +3,11 @@ import {Action_AssignAsTarget} from "./AssignAsTarget";
 export class Action_AssignNearestHostileCreepAsTarget extends Action_AssignAsTarget {
 
     public findTarget(creep: Creep) {
-        return creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS) as Creep;
+        let creep = creep.pos.findClosestByPath(FIND_HOSTILE_CREEPS) as Creep;
+        if (creep === undefined) {
+            creep = creep.pos.findClosestByRange(FIND_HOSTILE_CREEPS) as Creep;
+        }
+
+        return creep;
     }
 }
