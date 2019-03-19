d6020b488c78404e4187cf1156f58aace6d8aae9
Daniel Mecke <daniel.mecke@cunningsoft.de>
2019-03-19 20:08:16 +0100

    added simple console command to list creeps

--- src/consoleCommands.ts
+++ src/consoleCommands.ts
@@ -1,8 +1,27 @@
+import {Util_Logger} from "./Util/Logger";
+import {Role_Role} from "./Role/Role";
+
 let consoleCommands = function() {
-    global.foo = function() {
-        console.log("foo");
+
+    global.help = function() {
+        Util_Logger.debug("help");
+        Util_Logger.debug("creeps");
+
         return "";
-    }
+    };
+
+    global.creeps = function() {
+        for (let name in Game.creeps) {
+            if (Game.creeps.hasOwnProperty(name)) {
+                const creep = Game.creeps[name] as Creep;
+                const role = creep.role() as Role_Role;
+                Util_Logger.debug(creep.name + " - " + role.name());
+            }
+        }
+
+        return "";
+    };
+
 };
 
 export = consoleCommands;
