fe67316317b255d3ff49a96a1ec6c9e6f7f2c7b9
Daniel Mecke <daniel.mecke@cunningsoft.de>
2019-03-20 22:24:09 +0100

    added planning of spawn points

--- src/Task/StructurePlanning.ts
+++ src/Task/StructurePlanning.ts
@@ -4,10 +4,31 @@ export class Task_StructurePlanning extends Task_Task {
     public execute(): void {
         for (const name in Game.rooms) {
             if (Game.rooms.hasOwnProperty(name)) {
+                this.planSpawns(Game.rooms[name]);
                 this.planContainers(Game.rooms[name]);
                 this.planExtensions(Game.rooms[name]);
             }
         }
+    }
+
+    private planSpawns(room: Room): void {
+        const spawns = room.find(FIND_MY_SPAWNS) as StructureSpawn[];
+        if (spawns.length > 0) {
+            return;
+        }
+
+        const positions = room.find(FIND_SOURCES).map((source: Source) => source.pos) as RoomPosition[];
+        if (room.hasController()) {
+            positions.push(room.controller.pos);
+        }
+
+        const averagePosition = new RoomPosition(
+            _.sum(positions.map((position: RoomPosition) => position.x)) / positions.length + Math.floor(Math.random() * 10 - 5),
+            _.sum(positions.map((position: RoomPosition) => position.y)) / positions.length + Math.floor(Math.random() * 10 - 5),
+            room.name,
+        );
+
+        averagePosition.createConstructionSite(STRUCTURE_SPAWN);
     }
 
     private planContainers(room: Room): void {
